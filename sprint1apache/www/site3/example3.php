bb79e78c29d374ec5aae2b74a74f74e79a9b4148
adrianla117 <alopeza23@fpcoruna.afundacion.org>
2024-10-16 14:20:45 +0200

    Ejercicio 3: Crear otro .php en site3 para nuestro ejercicio 3. En el poner  con la edad para que cada vez que introduzcamos la edad en la URL cambie la pagina dependiendo.

---
+++ sprint1apache/www/site3/example3.php
@@ -0,0 +1,13 @@
+<html>
+<body>
+<h1>Jubilación</h1>
+<?php
+	
+	if ($_GET["edad"]+10 > 65) {
+		echo "En 10 años tendras edad de jubilación";
+	} else {
+		echo "¡Disfruta de tu tiempo!";
+	}
+?>
+</body>
+</html>
