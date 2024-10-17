97f111339531e50154dc198fe2046440d3f2fdcf
adrianla117 <alopeza23@fpcoruna.afundacion.org>
2024-10-17 12:10:38 +0200

    Ejercicio 6: Modificar el conversor2.php y añadir otra unidad de medida llamada pies. Para ello añadir un nuevo boton y al formulario y añadir otro if para la condicion y calculo de los pies.

--- sprint1apache/www/site3/conversor2.php
+++ sprint1apache/www/site3/conversor2.php
@@ -9,6 +9,10 @@
 					$v_pulgadas = $_POST["fcantidad"];
 					$v_metros = $v_pulgadas * 0.0254;
 					echo $v_pulgadas."pulgada(s) = ".$v_metros." metros(s)";
+				} elseif ($_POST["funidad"] == "pies") {
+					$v_pies = $_POST["fcantidad"];
+					$v_metros = $v_pies * 0.3048;
+					echo $v_pies."pie(s) = ".$v_metros." metro(s)";
 				} else {
 					echo "Unidad no soportada";
 				}
@@ -22,6 +26,8 @@
 
 			<input type="radio" id="pulgada_input" name="funidad" value="pulgada">
 			<label for="pulgada_input">Pulgada(s)</label><br>
+			<input type="radio" id="pies_input" name="funidad" value="pies">
+			<label for="pies_input">Pie(s)</label><br>
 			<input type="radio" id="otro_input" name="funidad" value="otro">
 			<label for="otro_input">Otro</label><br>
 
