9e52fca26da30deda1c931f19c9cf7c9f0e67042
adrianla117 <alopeza23@fpcoruna.afundacion.org>
2024-11-12 18:29:02 +0100

    Ejercicio 4: Modificar el archivo comment.php para que los usuarios puedan añadir comentarios cuando estan logeados

--- sprint3sesiones/www/mysite/comment.php
+++ sprint3sesiones/www/mysite/comment.php
@@ -1,23 +1,33 @@
 <?php
+	session_start();
+
 	$db = mysqli_connect('localhost', 'root', '1234', 'mysitedb') or die('Fail');
+
+	$id_pelicula = $_POST['pelicula_id'];
+	$comentario = $_POST['new_comment'];
+
+	$usuario_id = 'NULL';
+
+	if (isset($_SESSION['USER'])) {
+		$email = $_SESSION['user'];
+
+		$query_user = "SELECT id FROM usuarios WHERE email = '$email'";
+		$result_user = mysqli_query($db, $query_user);
+
+		if ($result_user && mysqli_num_rows($result_user) > 0) {
+			$row = mysqli_fetch_assoc($result_user);
+			$usuario_id = $row['id'];
+		}
+	}
+
+	$query = "INSERT INTO tComentarios (comentario, usuario_id, pelicula_id) VALUES ('$comentario', $usuario_id, $id_pelicula)";
+	mysqli_query($db, $query) or die('Error al insertar comentario');
+
+	echo "<p>Nuevo comentario ";
+	echo mysqli_insert_id($db);
+	echo " añadido</p>";
+
+	echo "<a href='/detail.php?pelicula_id=".$id_pelicula."'>Volver</a>";
+
+	mysqli_close($db);
 ?>
-
-<html>
-	<body>
-		<?php
-			$id_pelicula = $_POST['pelicula_id'];
-			$comentario = $_POST['new_comment'];
-
-			$query = "INSERT INTO tComentarios(comentario, usuario_id, pelicula_id) VALUES ('".$comentario."',NULL,".$id_pelicula.")";
-
-			mysqli_query($db, $query) or die('Error');
-
-			echo "<p>Nuevo comentario ";
-			echo mysqli_insert_id($db);
-			echo " añadido</p>";
-
-			echo "<a href='/detail.php?pelicula_id=".$id_pelicula."'>Volver</a>";
-			mysqli_close($db);
-		?>
-	</body>
-</html>
