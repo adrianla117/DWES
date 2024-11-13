74cc88205d8403612761e0a4c0ad946b09431325
adrianla117 <alopeza23@fpcoruna.afundacion.org>
2024-11-13 13:48:54 +0100

    Ejercicio 1: Crear un archivo .py que contendra las operaciones de suma, resta, multiplicacion, y division.

---
+++ sprint3python/operaciones.py
@@ -0,0 +1,17 @@
+def suma(a, b):
+    resultado = a + b
+    return resultado
+
+def resta(a, b):
+    resultado = a - b
+    return resultado
+
+def multiplicacion(a, b):
+    resultado = a * b
+    return resultado
+
+def division(a, b):
+    if b == 0:
+        return "Error: División por cero"
+    resultado = a / b
+    return resultado
